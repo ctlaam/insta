f17832b135c7982a7a21c2c063807a462e41fa74
Nguyễn Minh Công <mcong922@gmail.com>
2024-03-09 13:37:46 +0700

    sfgdgfd

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 header('Content-Type: application/json; charset=utf-8');
 require("./instagram.class.php");
 
+$proxies = ["207.228.3.97:46889@eo4iTcxrgWzYt0q:33EhWsSqbseKZZW"];
+
 $instaApi = new Instagram();
 
 $instaApi->sessionId = "39408242373%3ABfpl5LU4CozMRT%3A11%3AAYfXm2lyFZj3YIto8RMXu1zbm8greyAmX08SYA0Uwg;";
 
-$instaApi->proxy = "207.228.3.97:46889@eo4iTcxrgWzYt0q:33EhWsSqbseKZZW";
+$instaApi->proxy = array_rand($proxies);
 
 
 switch ($_GET["type"]) {
