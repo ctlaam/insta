aa91f119db023792e6d88373150f59e16b4cfe7b
Nguyễn Minh Công <mcong922@gmail.com>
2024-03-09 13:22:06 +0700

    fasd

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $instaApi = new Instagram();
 
 $instaApi->sessionId = "39408242373%3ABfpl5LU4CozMRT%3A11%3AAYfXm2lyFZj3YIto8RMXu1zbm8greyAmX08SYA0Uwg;";
 
-$instaApi->proxy = null;
+$instaApi->proxy = "207.228.3.97:46889@eo4iTcxrgWzYt0q:33EhWsSqbseKZZW";
 
 
 switch ($_GET["type"]) {
